e42e90b4439f754ecc39b8b6a8bc16f716cd65b1
Derek S <dereksilverman@gmail.com>
2013-11-22 14:54:03 -0500

    refactored and added method

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,10 +23,19 @@ class Scraper
 		html.search("h3").text.split(/(?<=[a-z.])(?=[A-Z])/)
 	end	
 
+
+	def get_name_wo_regex
+		all = html.search("h3")
+		all.collect do |h3|
+			h3.text
+		end
+	end	
+
 end
 
-#my_scraper = Scraper.new("http://flatironschool-bk.herokuapp.com/")
+my_scraper = Scraper.new("http://flatironschool-bk.herokuapp.com/")
 #puts my_scraper.get_twitter	
 #puts my_scraper.get_link
 #puts my_scraper.get_name
+puts my_scraper.get_name_wo_regex
 
